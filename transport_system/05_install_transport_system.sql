b4c499a2ac9350e04612e1e12eb65119fb8115ba
Roger Mähler <roger.mahler@hotmail.com>
2025-02-12 15:45:33 +0100

    feature: add procedure to create or update clearinghouse system with role management

--- transport_system/05_install_transport_system.sql
+++ transport_system/05_install_transport_system.sql
@@ -1,6 +1,19 @@
-
-do $$
+create or replace procedure clearing_house_commit.create_or_update_clearinghouse_system(
+    p_only_drop boolean = false,
+    p_dry_run boolean = false,
+    p_only_update boolean = true
+) as $$
 begin
+
+    set role clearinghouse_worker;
+
+    call clearing_house.create_public_model(p_only_drop, p_dry_run, p_only_update);
+
     perform clearing_house_commit.generate_sead_tables();
-    perform clearing_house_commit.generate_resolve_functions('public', false);
+    perform clearing_house_commit.generate_resolve_functions('public', p_dry_run);
+
+    reset role;
+    
 end $$ language plpgsql;
+
+call clearing_house_commit.create_or_update_clearinghouse_system(false, false, false);
